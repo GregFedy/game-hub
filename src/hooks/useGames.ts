c7a500389e164f1db46aeb7971a1a54ab5b39b7d
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-06-26 00:29:56 +0300

    feat: add loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,25 +23,29 @@ type FetchGamesResponse = {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
     const controller = new AbortController()
 
+    setLoading(true)
     apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results)
+        setLoading(false)
       })
       .catch((err) => {
         if (err.name !== 'CanceledError') {
-          setError(err.message);
+          setError(err.message)
+          setLoading(false)
         }
       })
 
     return () => controller.abort()
   }, [])
 
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
