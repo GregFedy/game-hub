a78da5836e81e966c0df60d6088ab42799da57c0
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-07-15 01:48:04 +0300

    feat: implement fetching game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,14 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
 import ms from 'ms'
 import APIClient, { type FetchResponse } from '../services/api-client'
-import { type Platform } from './usePlatforms'
 import useGameQueryStore from '../store/store'
+import { type Platform } from './usePlatforms'
 
 export type Game = {
   id: number
   name: string
+  slug: string
+  description_raw: string
   background_image: string
   parent_platforms: { platform: Platform }[]
   metacritic: number
