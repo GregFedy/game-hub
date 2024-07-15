f4e2a2c3750db23d917e0c12f786905b0594374f
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-07-16 01:37:01 +0300

    fix: add navigation logic

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,22 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 import useGameQueryStore from '../store/store'
 
 const SearchInput = () => {
   const inputRef = useRef<HTMLInputElement>(null)
   const setSearch = useGameQueryStore((s) => s.setSearch)
+  const navigate = useNavigate()
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault()
-        if (inputRef.current) setSearch(inputRef.current.value)
+        if (inputRef.current) {
+          setSearch(inputRef.current.value)
+          navigate('/')
+        }
       }}
     >
       <InputGroup>
