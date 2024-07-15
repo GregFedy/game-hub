5cc9652bb55b15c32cbe44fda98c04aa9c2dbfd9
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-07-15 18:32:31 +0300

    feat: add game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
-import type { Platform } from './Platform'
+import { type Genre } from './Genre'
+import { type Platform } from './Platform'
+import { type Publisher } from './Publisher'
 
 export type Game = {
   id: number
   name: string
   slug: string
+  genres: Genre[]
+  publishers: Publisher[]
   description_raw: string
   background_image: string
   parent_platforms: { platform: Platform }[]
